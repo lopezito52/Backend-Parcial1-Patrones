9bdcceacd43ec9790d374905b725c045e20f9d38
DNico21 <deividurla@unisabana.edu.co>
2026-03-02 19:50:01 -0500

    Correcion import course

--- pedido-backend/pedido-backend/src/main/java/com/grupoPatrones/pedido_backend/controller/ProductoController.java
+++ pedido-backend/pedido-backend/src/main/java/com/grupoPatrones/pedido_backend/controller/ProductoController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
